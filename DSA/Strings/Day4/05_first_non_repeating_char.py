d48766a481542e3983fcc35543b2b1b2d3227916
suzzith <sujithvyapare@gmail.com>
2025-07-04 15:11:32 +0530

    Add DSA Day 4 - first non-repeating character

---
+++ DSA/Strings/Day4/05_first_non_repeating_char.py
@@ -0,0 +1,10 @@
+s = "aaabccdeff"
+freq = {}
+
+for ch in s:
+    freq[ch] = freq.get(ch, 0) + 1
+
+for ch in s:
+    if freq[ch] == 1:
+        print("First non-repeating character:", ch)
+        break
